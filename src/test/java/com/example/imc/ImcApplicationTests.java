01ccd6edc370094be7fe16eaa33483d630ff555e
Yan Lemke de Castro <yan.castro@compass.uol>
2025-10-18 18:44:21 -0300

    create

---
+++ src/test/java/com/example/imc/ImcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
